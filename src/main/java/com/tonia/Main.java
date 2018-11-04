02915d42d657a9305514e944f632bf371a903d85
Task_1_1 <cooltone4ka@gmail.com>
2018-11-04 18:55:30 +0200

    Task_1_7.4

--- src/main/java/com/tonia/Main.java
+++ src/main/java/com/tonia/Main.java
@@ -7,16 +7,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         double k;
-        int a = 1;
         for (k = 2; k <= Math.sqrt(number); k++) {
             if (number % k == 0) {
-                a = 0;
                 System.out.println("Number isn't simple");
-                break;
+                return;
             }
         }
-        if (a == 1){
             System.out.println("Number is simple");
-        }
     }
 }
